b3f0cec83ce9f1f1a40c7e9a0ac61f0594c77fae
amanaligit <ali.aman.2010@gmail.com>
2021-04-09 20:58:48 +0530

    test

--- servers.js
+++ servers.js
@@ -46,7 +46,7 @@ if (cluster.isMaster) {
     };
 
     // Spawn workers.
-    for (var i = 0; i < 4; i++) {
+    for (var i = 0; i < num_processes / 2; i++) {
         console.log("spawning", i)
         spawn(i);
     }
@@ -73,7 +73,7 @@ if (cluster.isMaster) {
         // We received a connection and need to pass it to the appropriate
         // worker. Get the worker for this connection's source IP and pass
         // it the connection.
-        let worker = workers[worker_index(connection.remoteAddress, 4)];
+        let worker = workers[worker_index(connection.remoteAddress, num_processes / 2)];
         worker.send('sticky-session:connection', connection);
     });
     server.listen(port);
