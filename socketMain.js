71d18e986c2dfd60bf22bae3cbc7ea427f3a6e92
amanaligit <ali.aman.2010@gmail.com>
2021-04-09 21:27:56 +0530

    changes

--- socketMain.js
+++ socketMain.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-require('dotenv').config()
 const uri = process.env.DATABASE_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
